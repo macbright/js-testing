641cb316321e08976ce8a06372ae06839d2f5d98
addo <forison6205@gmail.com>
2019-11-29 07:58:56 -0500

    added: readme

--- functions.test.js
+++ functions.test.js
@@ -39,20 +39,20 @@ test('cipher decryption', () => {
 
 test('analyzeArray ', () => {
   expect(functions.analyzeArray([6, 7, 2, 5])).toEqual({
-		average:5,
-		min:2,
-		max:7,
-		length:4
-		})
-  
+    average: 5,
+    min: 2,
+    max: 7,
+    length: 4
+  })
+
 });
 
 test('analyzeArray ', () => {
-  expect(functions.analyzeArray([1,8,3,4,2,6])).toEqual({
-		average: 4,
-		min: 1,
-		max: 8,
-		length: 6
-		})
-  
+  expect(functions.analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  })
+
 });
