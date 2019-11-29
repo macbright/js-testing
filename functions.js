1be78e53d15976abc7f32c4d32b1f4ecfb6ed5f1
bright okike <brightokike@gmail.com>
2019-11-29 15:52:53 +0300

    linter error

--- functions.js
+++ functions.js
@@ -1,53 +1,49 @@
 
-/* global test, expect*/
-/* eslint no-undef: "error" */
-
 const functions = {
   capitalize: string => string[0].toUpperCase() + string.slice(1),
   reverseString: (string) => string.split('').reverse().join(''),
-  calculator: {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    divide: (a, b) => a / b,
-    multiply: (a, b) => a * b
-  },
-  encrypt: ((string, key) => {
-    const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const encrypted = [];
-    string.toLowerCase().split('')
-      .map((value) => {
-        abc.indexOf(value) === -1 ?
-          value === ' ' ? encrypted.push(' ') : false
-          :
-          encrypted.push(abc[(abc.indexOf(value) + key) % 26])
-      });
-    return encrypted.join('');
-  }),
-  decrypt: ((string, key) => {
-    const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const decrypted = [];
-    string.toLowerCase().split('')
-      .map((value) => {
-        abc.indexOf(value) === -1 ?
-          value === ' ' ? decrypted.push(' ') : false
-          :
-          (abc.indexOf(value) - key) % 26 < 0 ?
-            decrypted.push(abc[((abc.indexOf(value) - key) % 26) + 26])
-            :
-            decrypted.push(abc[((abc.indexOf(value) - key) % 26)]);
-      });
-    return decrypted.join('');
-  }),
+	calculator: {
+		add: (a, b) => a + b,
+		subtract: (a, b) => a - b,
+		divide: (a, b) => a / b,
+		multiply: (a, b) => a * b
+	},
+	encrypt: ((string, key) => {
+		const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+		const encrypted = [];
+		string.toLowerCase().split('')
+			.map((value) => {
+				abc.indexOf(value) === -1 ?
+				value === ' ' ? encrypted.push(' ') : false
+					:
+  				encrypted.push(abc[(abc.indexOf(value) + key) % 26])
+			});
+		return encrypted.join('');
+	}),
+	decrypt: ((string, key) => {
+		const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+		const decrypted = [];
+		string.toLowerCase().split('')
+  		.map((value) => {
+  		abc.indexOf(value) === -1 ?
+				value === ' ' ? decrypted.push(' ') : false
+					:
+					(abc.indexOf(value) - key) % 26 < 0 ?
+						decrypted.push(abc[((abc.indexOf(value) - key) % 26) + 26])
+						:
+						decrypted.push(abc[((abc.indexOf(value) - key) % 26)]);
+			});
+		return decrypted.join('');
+	}),
 	analyzeArray: (arr) => {
-    return {
-      average: Math.floor(arr.reduce((sum, num) => {
-        return sum + num;
-      }) / arr.length),
-      min: Math.min(...arr),
-      max: Math.max(...arr),
-      length: arr.length,
+		return {
+			average: Math.floor(arr.reduce((sum, num) => {
+				return sum + num;
+			}) / arr.length),
+			min: Math.min(...arr),
+			max: Math.max(...arr),
+			length: arr.length,
     }
   }
-};
-
+};	
 module.exports = functions;
