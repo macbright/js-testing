f4f01aa80989b23a4af4b12f4634090562e89cab
addo <forison6205@gmail.com>
2019-11-29 07:15:57 -0500

    added: caesar cipher method and test

--- functions.js
+++ functions.js
@@ -1,13 +1,39 @@
 
 const functions = {
-	capitalize: string => string[0].toUpperCase() + string.slice(1),
+  capitalize: string => string[0].toUpperCase() + string.slice(1),
   reverseString: (string) => string.split('').reverse().join(''),
   calculator: {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
     divide: (a, b) => a / b,
     multiply: (a, b) => a * b
-  }
-}
-
+  },
+  encrypt: ((string, key) => {
+    const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    encrypted = [];
+    string.toLowerCase().split('')
+      .map((value) => {
+        abc.indexOf(value) === -1 ?
+          value === ' ' ? encrypted.push(' ') : false
+          :
+          encrypted.push(abc[(abc.indexOf(value) + key) % 26])
+      });
+    return encrypted.join('')
+  }),
+  decrypt: ((string, key) => {
+    const abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    encrypted = [];
+    string.toLowerCase().split('')
+      .map((value) => {
+        abc.indexOf(value) === -1 ?
+          value === ' ' ? encrypted.push(' ') : false
+          :
+          (abc.indexOf(value) - key) % 26 < 0 ?
+            encrypted.push(abc[((abc.indexOf(value) - key) % 26) + 26])
+            :
+            encrypted.push(abc[((abc.indexOf(value) - key) % 26)]);
+      });
+    return encrypted.join('');
+  })
+};
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -2,9 +2,34 @@
 const functions = require('./functions');
 
 test('capitalize should Capitalize the string', () => {
-  expect(functions.capitalize('you')).toBe('You')
+  expect(functions.capitalize('you')).toBe('You');
 });
 
 test('reverseString should reverse the string', () => {
-  expect(functions.reverseString('amaka')).toBe('akama')
+  expect(functions.reverseString('amaka')).toBe('akama');
+});
+
+test('calculate by adding two numbers', () => {
+  expect(functions.calculator.add(1, 2)).toBe(3);
+});
+
+test('calculate by substracting two numbers', () => {
+  expect(functions.calculator.subtract(6, 5)).toBe(1);
+});
+
+test('calculate by dividing two numbers', () => {
+  expect(functions.calculator.divide(6, 2)).toBe(3);
+});
+
+test('calculate by multiplying two numbers', () => {
+  expect(functions.calculator.multiply(6, 2)).toBe(12);
+});
+
+test('cipher encryption', () => {
+  expect(functions.encrypt("a and, b", 1)).toBe("b boe c");
+  expect(functions.encrypt("hello world", 5)).toBe("mjqqt btwqi");
+});
+test('cipher decryption', () => {
+  expect(functions.decrypt("b boe, c", 1)).toBe("a and b");
+  expect(functions.decrypt("mjqqt btwqi", 5)).toBe("hello world");
 });
